302b19f5c155630e3bb434be152d39bbe5a3eb4b
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2018-07-10 15:03:20 +0100

    Update knex file

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const config = require('./server/config');
 module.exports = {
     client: 'pg',
     connection: {
-        server: config.db.server,
+        host: config.db.server,
         port: config.db.port,
         user: config.db.username,
         password: config.db.password,
